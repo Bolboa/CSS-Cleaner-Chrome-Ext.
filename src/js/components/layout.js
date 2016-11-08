e03ba65320d48db257529cfcab0718e5c6a53639
Eric Bolboa <13eb10@queensu.ca>
2016-11-08 17:02:48 -0500

    get html source code

--- src/js/components/layout.js
+++ src/js/components/layout.js
@@ -3,9 +3,31 @@ import React from 'react';
 export default class Layout extends React.Component {
 	constructor(){
 		super();
-		this.state = {
-			title:'welcome'
-		};
+	}
+
+
+	componentWillMount(){
+		console.log("hello");
+		var result = '';
+	    chrome.tabs.executeScript(null, {
+	        file: 'src/js/scripts/getPageSource.js'
+	     }, function() {
+	     	console.log("batcch");
+	        // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+	        if (chrome.runtime.lastError) {
+	            result = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+	            console.log(result);
+	        }
+	    });
+	}
+
+	componentDidMount() {
+		chrome.runtime.onMessage.addListener(function(request, sender) {
+  			if (request.action == "getSource") {
+    			console.log(request.source);
+  			}
+		});
+
 	}
 
 	render() {
