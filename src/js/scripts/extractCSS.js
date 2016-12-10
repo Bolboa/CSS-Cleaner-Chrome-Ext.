24a9e1dfd3df56fdf61d0368edd7f7c59e864d06
Eric Bolboa <13eb10@queensu.ca>
2016-12-10 03:14:30 -0500

    keyframes and media queries are cleaned/ Documentation

--- src/js/scripts/extractCSS.js
+++ src/js/scripts/extractCSS.js
@@ -90,100 +90,168 @@ function cleanCSS(stylesheet, sourceCode) {
 /*------------------GET STRING OF MAPPED CSS-------------*/
 function stringMappedCSS(stylesheet, mapping) {
 
+    //reserved for full cleaned version of CSS
     var newCSS = '';
     var oldCSS = '';
+
+    //mapping array for regular CSS identifiers
     var mappingArray = mapping[0];
+    //HTML source code classes
     var htmlSource = mapping[1];
+    //HTML source code Ids
     var htmlsourceId = mapping[2];
-    var before = mappingArray.length;
+
+    //the length of CSS file before the cleanse
+    var before = 0;
+    //the length of CSS file after cleanse
     var after = 0;
+
+    //array reserved for storing keyframes
+    var keyframes = [];
+
+    //loop through original stylesheet selected by the user
     for (var i=0; i<stylesheet.cssRules.length; i++) {
+        //append all attributes to the oldCSS variable
         oldCSS += stylesheet.cssRules[i].cssText;
+        //if regular identifier is true (1)
         if (mappingArray[i] == 1) {
             newCSS += stylesheet.cssRules[i].cssText;
+            newCSS += '\n';
             after += 1;
         }
+        //if media query or keyframe (2)
         else if (mappingArray[i] == 2) {
-            cleanMediaQuery(stylesheet.cssRules[i].cssText, htmlSource, htmlsourceId);
+            //function to return the cleaned version of the media query
+            var cleanedMedia = cleanMediaQuery(stylesheet.cssRules[i].cssText, htmlSource, htmlsourceId);
+            if (cleanedMedia != false) {
+                //if result is media query
+                if (cleanedMedia[0] == 'media') {
+                    newCSS += cleanedMedia[1];
+                    newCSS += '\n';
+                }
+                //if result is keyframe
+                else if (cleanedMedia[0] == 'key') {
+                    keyframes.push(cleanedMedia[1]);
+                }
+            }
         }
     }
-   
+
+    //get an array of all attributes that call a keyframe
+    var animations = newCSS.match(/animation(.*?);/g);
+    //returns all keyframes in use by the cleaned CSS file
+    var cleanedKeyframes = keyframeClean(animations, keyframes);
+
+    //append only the keyframes being used
+    for (var i=0; i<cleanedKeyframes.length; i++) {
+        newCSS += cleanedKeyframes[i];
+    }
+
     //Calculate how much CSS was removed
+    before = oldCSS.length
+    after = newCSS.length;
     var percentage = 100 - ((after/before)*100);
     percentage = Math.round(percentage*100)/100;
     
     return [newCSS, percentage];
-    
+}
 
+
+
+/*-----------------RETURN KEYFRAMES BEING USED--------------------------*/
+function keyframeClean(animationCalls, keyframes) {
+
+    //reserved for storing only necessary keyframes
+    var keyframesToKeep = [];
+    //loop through all animations used in the CSS
+    for (var i=0; i< animationCalls.length; i++) {
+        //loop through all keyframes in use in current stylesheet
+        for (var j=0; j<keyframes.length; j++) {
+            //extract only the animation name
+            var keyframeStyle = keyframes[j].match(/keyframes(.*?){/);
+            keyframeStyle = keyframeStyle[1].trim();
+            //if animation is being called by an CSS animation attribute, keep it
+            if (animationCalls[i].indexOf(keyframeStyle) !== -1) {
+                keyframesToKeep.push(keyframes[j]);
+            }
+        }
+    }
+    return keyframesToKeep;
 }
 
 /*-------------RETURN CLEAN VERSION OF MEDIA QUERY---------------------*/
 function cleanMediaQuery(mediaQuery, htmlsource, htmlsourceId) {
+
     //the begining of the media query
     var media_start = mediaQuery.match(/@media(.*?){/);
     //this will be the content of the media query
     var media_content = '';
 
+    //if media_start == null, it is a keyframe
     if (mediaQuery != null && media_start != null) {
+        //remove newlines
         var trim_media = mediaQuery.replace(/(\r\n|\n|\r)/gm,"");
         var media_start = media_start[0];
         
+        //get all class elements from media query
         var identifiers = mediaQuery.match(/\.(.*?)}/g);
+        //get all id elements from media query
         var id_identifiers = mediaQuery.match(/\#(.*?)}/g);
+        
         if (identifiers != null && media_start != null) {
             for (var i=0; i<identifiers.length; i++) {
-
+                //extract only the class names
                 var class_identifier = identifiers[i].match(/\.(.*){/);
 
                 if (class_identifier != null) {
                     class_identifier = class_identifier[1].trim();
                     class_identifier = "." + class_identifier;
+                    //check if class name exists in the html source code, if this does keep it (along with the styles) in the media query
                     for (var j=0; j<htmlsource.length; j++) {
                         if (class_identifier.indexOf(htmlsource[j]) !== -1 && htmlsource[j]) {
                             media_content += " ";
                             media_content += identifiers[i];
-                            //console.log(identifiers[i]);
                             break;
-                          
                         }
-                      
                     }
                 }
-               
             }
         }
+
         if (id_identifiers != null && media_start != null) {
             for (var i=0; i<id_identifiers.length; i++) {
+                //extract only Id name
                 var id_identifier = id_identifiers[i].match(/\#(.*){/);
+
                 if (id_identifier != null) {
                     id_identifier = id_identifier[1].trim();
                     id_identifier = "#" + id_identifier;
-                    //console.log(id_identifier);
+                    //check if Id name exists in the html source code, if this does keep it (along with the styles) in the media query
                     for (var j=0; j<htmlsourceId.length; j++) {
                         if (id_identifier.indexOf(htmlsourceId[j]) !== -1 && htmlsourceId[j]) {
                             media_content += " ";
                             media_content += id_identifier[i];
-
                             break;
-                          
                         }
                     }
                 }
             }
-        
         }
+
+        //only return if content is not empty to prevent the return of an empty media query
         if (media_content) {
             media_start += media_content;
             media_start += '}';
-            console.log(media_start);
-
+            return ['media', media_start];
         }
-            
-
-        
-        
+    }
+    //return keyframe
+    else {
+        return ['key', mediaQuery];
     }
 
+    //default return
+    return false;
 }
 
 
--- src/js/scripts/getPageSource.js
+++ src/js/scripts/getPageSource.js
@@ -1,35 +1,24 @@
 
-function DOMtoString(document_root) {
 
-    var classes = [],
-        elIds = [],
-        all = document_root.getElementsByTagName("*");
-    for (var i=0, max=all.length; i < max; i++) {
-        if (all[i].className) {
-            classes.push(all[i]);
-        }
-        if (all[i].id) {
-            elIds.push(all[i]);
-        }
+/*------------RETURNS ALL STYLESHEETS IN USE ON THE PAGE---------*/
+function listOfCSS(document_root) {
 
-    }
-    
     //Get list of stylesheets being used on page
     var stylesheet = document_root.styleSheets[(document_root.styleSheets.length - 1)];
     var styleList = [];
     
+    //gets the array of stylesheets
     for( var i in document_root.styleSheets ){
         stylesheet = document_root.styleSheets[i];
         styleList.push(stylesheet.href);
-        
     }
 
-    var html = {styles:styleList};
-
-    return html;
+    var CSS = {styles:styleList};
+    return CSS;
 }
 
+//return the list of stylesheets to the chrome extension
 chrome.runtime.sendMessage({
     action: "getSource",
-    source: DOMtoString(document)
+    source: listOfCSS(document)
 });
