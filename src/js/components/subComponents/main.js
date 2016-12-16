e70ea5294d667ce66aad0837ca07b83ce0e4192a
Eric Bolboa <13eb10@queensu.ca>
2016-12-16 00:16:52 -0500

    Update main.js

--- src/js/components/subComponents/main.js
+++ src/js/components/subComponents/main.js
@@ -9,29 +9,30 @@ export default class Main extends Component{
 		window.close();
 	}
 	render(){
-        return(
+        	return(
+			
         	<div className="default">
-	            <div className="cssList">
-					<h1>Select the stylesheet you wish to clean</h1>
-					{
-						this.props.css.map((style) => {
-							if (style) {
-								return (
-									<div className="inputWrap">
-										<input type="radio" name="style_name" onClick={() => this.props.select(style)}/>
-										<span></span>
-										<a key={style}>{style}</a>
-									</div>
+	            	<div className="cssList">
+				<h1>Select the stylesheet you wish to clean</h1>
+				{
+					this.props.css.map((style) => {
+						if (style) {
+							return (
+								<div className="inputWrap">
+									<input type="radio" name="style_name" onClick={() => this.props.select(style)}/>
+									<span></span>
+									<a key={style}>{style}</a>
+								</div>
 								);
 							}
 						})
 					}
 					
-					</div>
-					<button className="cleanBtn" onClick={this.props.save}>Clean!</button>
-					<i class="fa fa-times-circle" aria-hidden="true" onClick={this.close.bind(this)}></i>
-				</div>
-        )
+			</div>
+			<button className="cleanBtn" onClick={this.props.save}>Clean!</button>
+			<i class="fa fa-times-circle" aria-hidden="true" onClick={this.close.bind(this)}></i>
+		</div>
+        	)
 
-    }
+	}
 }
