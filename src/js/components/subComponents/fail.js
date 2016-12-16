cf4702d6803be612c0b7593af47b348f5ffeee3c
Eric Bolboa <13eb10@queensu.ca>
2016-12-16 00:20:08 -0500

    Update fail.js

--- src/js/components/subComponents/fail.js
+++ src/js/components/subComponents/fail.js
@@ -14,14 +14,15 @@ export default class Fail extends Component{
 
         	return(
 
-        	<div className="fail">
-        		<i class="fa fa-times" aria-hidden="true"></i>
-        		<div className="text">
-	        		<h1>Error</h1>
-	        		<div className="errMsg">Stylesheet is unreachable!</div>
-	        	</div>
-	        	<i class="fa fa-times-circle" aria-hidden="true" onClick={this.close.bind(this)}></i>
-		</div>
+			<div className="fail">
+				<i class="fa fa-times" aria-hidden="true"></i>
+				<div className="text">
+					<h1>Error</h1>
+					<div className="errMsg">Stylesheet is unreachable!</div>
+				</div>
+				<i class="fa fa-times-circle" aria-hidden="true" onClick={this.close.bind(this)}></i>
+			</div>
+
 		)
 	}
 }
