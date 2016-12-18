6f8baca2989c9d3c23de70c803a143c876c09791
Eric Bolboa <13eb10@queensu.ca>
2016-12-18 14:58:33 -0500

    Update success.js

--- src/js/components/subComponents/success.js
+++ src/js/components/subComponents/success.js
@@ -11,16 +11,16 @@ export default class Success extends Component{
 	}
 
 	render(){
-        	return(
-				<div className="success">
-					<i class="fa fa-check" aria-hidden="true"></i>
-					<div className="text">
-						<h1>SUCCESS!</h1>
-						{/*HOW MUCH CSS WAS REMOVED*/}
-						<div className="stat">{this.props.removedStat}% of CSS was removed!</div>
-					</div>
-					<i class="fa fa-times-circle" aria-hidden="true" onClick={this.close.bind(this)}></i>
+        return(
+			<div className="success">
+				<i class="fa fa-check" aria-hidden="true"></i>
+				<div className="text">
+					<h1>SUCCESS!</h1>
+					{/*HOW MUCH CSS WAS REMOVED*/}
+					<div className="stat">{this.props.removedStat}% of CSS was removed!</div>
 				</div>
+				<i class="fa fa-times-circle" aria-hidden="true" onClick={this.close.bind(this)}></i>
+			</div>
 		)
 	}
 }
